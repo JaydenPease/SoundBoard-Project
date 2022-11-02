9c5b9093c408932230bcf087d95bf3b3c72fefd9
JaydenPease <jaydenpease@gmail.com>
2022-11-02 10:08:52 -0700

    the thing

--- app/src/main/java/com/mistershorr/soundboard/MainActivity.kt
+++ app/src/main/java/com/mistershorr/soundboard/MainActivity.kt
@@ -6,22 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.mistershorr.soundboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
-    lateinit var buttonA : Button
-    lateinit var buttonBb : Button
-    lateinit var buttonB : Button
-    lateinit var buttonC : Button
-    lateinit var buttonCs: Button
-    lateinit var buttonD: Button
-    lateinit var buttonDs: Button
-    lateinit var buttonE: Button
-    lateinit var buttonF: Button
-    lateinit var buttonFs: Button
-    lateinit var buttonG: Button
-    lateinit var buttonGs: Button
+
+
+    //instance variable for the view binding
+    private lateinit var binding: ActivityMainBinding
     
     lateinit var soundPool : SoundPool
     var ANote = 0
@@ -40,28 +33,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // define the binding  variable
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        wireWidgets()
+
         initializeSoundPool()
         setListeners()
     }
 
     private fun setListeners() {
         val soundBoardListener = SoundBoardListener()
-        buttonA.setOnClickListener(soundBoardListener)
-        buttonBb.setOnClickListener(soundBoardListener)
-        buttonB.setOnClickListener(soundBoardListener)
-        buttonC.setOnClickListener(soundBoardListener)
+        binding.buttonMainA.setOnClickListener(soundBoardListener)
+        binding.buttonMainBb.setOnClickListener(soundBoardListener)
+        binding.buttonMainB.setOnClickListener(soundBoardListener)
+        binding.buttonMainC.setOnClickListener(soundBoardListener)
+        binding.buttonMainCs.setOnClickListener(soundBoardListener)
+        binding.buttonMainD.setOnClickListener(soundBoardListener)
+        binding.buttonMainDs.setOnClickListener(soundBoardListener)
+        binding.buttonMainE.setOnClickListener(soundBoardListener)
+        binding.buttonMainF.setOnClickListener(soundBoardListener)
+        binding.buttonMainFs.setOnClickListener(soundBoardListener)
+        binding.buttonMainG.setOnClickListener(soundBoardListener)
+        binding.buttonMainGs.setOnClickListener(soundBoardListener)
+        binding.buttonMainLg.setOnClickListener((soundBoardListener))
     }
 
 
-    private fun wireWidgets() {
-        buttonA = findViewById(R.id.button_main_a)
-        buttonBb = findViewById(R.id.button_main_bb)
-        buttonB = findViewById(R.id.button_main_b)
-        buttonC = findViewById(R.id.button_main_c)
-    }
+
 
     private fun initializeSoundPool() {
 
@@ -74,6 +73,14 @@ class MainActivity : AppCompatActivity() {
         BbNote = soundPool.load(this, R.raw.scalebb, 1)
         BNote = soundPool.load(this, R.raw.scaleb, 1)
         CNote =  soundPool.load(this, R.raw.scalec, 1)
+        CsNote = soundPool.load(this, R.raw.scalecs, 1)
+        DNote = soundPool.load(this, R.raw.scaled, 1)
+        DsNote = soundPool.load(this, R.raw.scaleds, 1)
+        ENote = soundPool.load(this, R.raw.scalee, 1)
+        FNote = soundPool.load(this, R.raw.scalef, 1)
+        FsNote = soundPool.load(this, R.raw.scalefs, 1)
+        GNote = soundPool.load(this, R.raw.scaleg, 1)
+        GsNote = soundPool.load(this, R.raw.scalegs, 1)
     }
 
     private fun playNote(noteId : Int) {
@@ -87,7 +94,33 @@ class MainActivity : AppCompatActivity() {
                 R.id.button_main_bb -> playNote(BbNote)
                 R.id.button_main_b -> playNote(BNote)
                 R.id.button_main_c -> playNote(CNote)
+                R.id.button_main_cs -> playNote(CsNote)
+                R.id.button_main_d -> playNote(DNote)
+                R.id.button_main_ds -> playNote(DsNote)
+                R.id.button_main_e -> playNote(ENote)
+                R.id.button_main_f -> playNote(FNote)
+                R.id.button_main_fs -> playNote(FsNote)
+                R.id.button_main_g -> playNote(GNote)
+                R.id.button_main_gs -> playNote(GsNote)
             }
         }
+
+    }
+
+    private fun playSong(song: List<Note>) {
+        //loop through a list of notes
+          //play the note
+                //note you get is a string
+                //to play the note, you need the corresponding soundPool object
+           //delay for the delay
+    }
+
+    private fun delay(time: Long) {
+        try{
+            Thread.sleep(time)
+        } catch(e: InterruptedException) {
+            e.printStackTrace()
+        }
+
     }
 }
---
+++ app/src/main/java/com/mistershorr/soundboard/Note.kt
@@ -0,0 +1,3 @@
+package com.mistershorr.soundboard
+
+data class Note(val note: String, val delay: Long)

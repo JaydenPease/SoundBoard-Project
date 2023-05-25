cb7483d034716485bf66c71610b63eb3c6c85efc
jayden pease <jaydenpease@gmail.com>
2023-05-25 09:50:16 -0700

    finished

--- app/src/main/java/com/mistershorr/soundboard/Song.kt
+++
@@ -1,7 +0,0 @@
-package com.mistershorr.soundboard
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Song(var title: String, var notes: ArrayList<Note>): Parcelable
